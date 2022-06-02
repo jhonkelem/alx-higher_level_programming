2584f127489e1823e86bc047bdff3067de2e816d
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-06-02 15:35:48 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python
 
-if __name__ == '__main__':
+from calculator_1 import add, sub, mul, div
+
+
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
     import sys
-    from calculator_1 import add, sub, mul, div
-
-    argc = len(sys.argv)
-    result = 0
-    if argc != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
-    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and\
-       sys.argv[2] != '/':
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-
-    if sys.argv[2] == '+':
-        result = add(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == '-':
-        result = sub(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == '*':
-        result = mul(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == '/':
-        result = div(int(sys.argv[1]), int(sys.argv[3]))
-    print('{:d} {} {:d} = {:d}'.format(int(sys.argv[1]), sys.argv[2],
-                                       int(sys.argv[3]), result))
+    arg_calc(sys.argv)
