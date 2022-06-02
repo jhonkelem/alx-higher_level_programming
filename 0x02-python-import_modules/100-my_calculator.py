32f103be7188652f183c645a34c5f33bfc4d5b42
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-06-02 15:23:05 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,42 +5,22 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
 
     argc = len(sys.argv)
+    result = 0
+    if argc != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and\
+       sys.argv[2] != '/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
 
-        result = 0
-
-            if argc != 4:
-
-                        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-
-                                exit(1)
-
-                                    if sys.argv[2] != '+' and sys.argv[2] != '-' and sys.argv[2] != '*' and\
-
-                                                   sys.argv[2] != '/':
-
-                                                               print('Unknown operator. Available operators: +, -, * and /')
-
-                                                                       exit(1)
-
-
-
-                                                                           if sys.argv[2] == '+':
-
-                                                                                       result = add(int(sys.argv[1]), int(sys.argv[3]))
-
-                                                                                           elif sys.argv[2] == '-':
-
-                                                                                                       result = sub(int(sys.argv[1]), int(sys.argv[3]))
-
-                                                                                                           elif sys.argv[2] == '*':
-
-                                                                                                                       result = mul(int(sys.argv[1]), int(sys.argv[3]))
-
-                                                                                                                           elif sys.argv[2] == '/':
-
-                                                                                                                                       result = div(int(sys.argv[1]), int(sys.argv[3]))
-
-                                                                                                                                           print('{:d} {} {:d} = {:d}'.format(int(sys.argv[1]), sys.argv[2],
-
-                                                                                                                                                                                      int(sys.argv[3]), result))
-
+    if sys.argv[2] == '+':
+        result = add(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '-':
+        result = sub(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '*':
+        result = mul(int(sys.argv[1]), int(sys.argv[3]))
+    elif sys.argv[2] == '/':
+        result = div(int(sys.argv[1]), int(sys.argv[3]))
+    print('{:d} {} {:d} = {:d}'.format(int(sys.argv[1]), sys.argv[2],
+                                       int(sys.argv[3]), result))
