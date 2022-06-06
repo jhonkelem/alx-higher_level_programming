d0c0f291366ec65a8b69d54913a48994bd284359
root <yohanneskelem@gmail.com>
2022-06-06 01:50:54 -0700

    tasks on python data structures,list,tupels

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,4 @@
 
 def print_list_integer(my_list=[]):
     for num in my_list:
-        print('{:d}'.format(num))
-
-#return print('{}'.format(my_list))        
+        print('{:d}'.format(num))        
