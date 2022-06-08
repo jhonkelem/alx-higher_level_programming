6f556515b7ec20871e4cea720b5673057fc036bd
root <yohanneskelem@gmail.com>
2022-06-08 06:28:07 -0700

    tasks on python more data structures:set,dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        sorted_dict = sorted(a_dictionary.items(), key=lambda item: item[1])
+        return (sorted_dict[-1])[0]
+    else:
+        return None
