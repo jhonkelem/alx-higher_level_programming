fd38d91b176ad05b986e284ca4108adfd8220758
root <yohanneskelem@gmail.com>
2022-06-08 13:21:17 -0700

    tasks on python more data structures:set, dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def simple_delete(my_dict, key=""):
-    if key in my_dict:
-        del my_dict[key]
-        return (my_dict)
+def simple_delete(a_dictionary, key=""):
+    try:
+        a_dictionary.pop(key)
+    except KeyError:
+        pass
+    return a_dictionary
