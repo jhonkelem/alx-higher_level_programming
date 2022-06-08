567e89f22e64e485ea9e5575b99b275cd30a78f3
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-06-08 23:42:54 +0200

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    try:
-        delete = []
-        for key in a_dictionary:
-            if a_dictionary[key] == value:
-                delete.append(key)
-                for key in delete:
-                    a_dictionary.pop(key)
-    except (KeyError, RuntimeError):
-        pass
-    return a_dictionary
+def complex_delete(my_dict, value):
+    targets = []
+    for key, key_value in my_dict.items():
+        if key_value is value:
+            targets.append(key)
+    for x in targets:
+        del my_dict[x]
+    return(my_dict)
