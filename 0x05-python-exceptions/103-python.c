089ed8341dd356102505d0e8b07b4796078316ea
root <yohanneskelem@gmail.com>
2022-06-20 07:47:05 -0700

    tasks on python exception

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - print some basic info about python bytes objects
+ * @p: python object
+ * Return: nothing
+ */
+
+void print_python_bytes(pyObject *p)
+{
+}
+
+/**
+ * print_python_float - print python float
+ * @p: python float
+ * Return: nothing
+ */
+
+void print_python_float(pyObject *p)
+{
+}
+
+/**
+ * print_python_list - print some basic info about python lists
+ * @p: python object
+ * Return: nothing
+ */
+
+void print_python_list(pyObject *p)
+{
+}
