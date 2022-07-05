c86bede7c56b0e26e352be771fca2f2cd701c310
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-07-05 13:17:47 +0200

    Update 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """
+    empty class
+    """
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception('area() is not implemented')
