28514ab29495ee89a916e027ece852bfb0848b1a
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-07-05 12:11:38 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """
-9-add_item module
+11-student module
 """
 
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-
-def add_items():
+class Student:
     """
-    function that adds all arguments to a Python list
-    and then saves them to a file
+    define the Student class
     """
-    try:
-        python_list = load_from_json_file('add_item.json')
-    except:
-        python_list = []
 
-    for i in range(1, len(sys.argv)):
-        python_list.append(sys.argv[i])
+    def __init__(self, first_name, last_name, age):
+        """initialize variables and methods"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    save_to_json_file(python_list, 'add_item.json')
-
-if __name__ == '__main__':
-    add_items()
+    def to_json(self):
+        """ """
+        return self.__dict__
