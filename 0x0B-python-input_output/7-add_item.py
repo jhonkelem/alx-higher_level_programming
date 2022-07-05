6c0a6e79c710a17ecbb43b3f6944cfa4c74b858c
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-07-05 12:07:55 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 """
-7-save_to_json_file module
+9-add_item module
 """
 
-import json
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
+def add_items():
     """
-    function that writes an Object to a text file, using a JSON representation
+    function that adds all arguments to a Python list
+    and then saves them to a file
     """
-    with open(filename, "w") as f:
-        f.write(json.dumps(my_obj))
+    try:
+        python_list = load_from_json_file('add_item.json')
+    except:
+        python_list = []
+
+    for i in range(1, len(sys.argv)):
+        python_list.append(sys.argv[i])
+
+    save_to_json_file(python_list, 'add_item.json')
+
+if __name__ == '__main__':
+    add_items()
