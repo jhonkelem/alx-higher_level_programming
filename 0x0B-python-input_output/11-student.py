e26851e28bd10a8319fd77e48b402d3a48b269ca
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-07-05 12:13:04 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-11-student module
+13-student module
 """
 
 
 class Student:
     """
-    define the Student class
+    Define the Student class
     """
 
     def __init__(self, first_name, last_name, age):
@@ -15,6 +15,22 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """
+        function that retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            a_dict = {}
+            for attr, value in (self.__dict__).items():
+                if attr in attrs:
+                    a_dict[attr] = value
+            return a_dict
+
+    def reload_from_json(self, json):
+        """function that replaces all attributes of the Student instance"""
+        for attr, value in json.items():
+            setattr(self, attr, value)
+#            self.attr = value
