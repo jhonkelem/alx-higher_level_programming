1415811866f4fce6af909f79959252a2548ee037
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-07-05 12:01:51 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """
-1-number_of_lines module
+3-write_file module
 """
 
 
-def number_of_lines(filename=""):
-    """function that returns the number of lines of a text file"""
-    with open(filename, 'r') as f:
-        return len(f.readlines())
+def write_file(filename="", text=""):
+    """
+    function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+    """
+    with open(filename, 'w') as wf:
+        return wf.write(str(text))
+#    with open(filename, 'r') as rf:
+#        rf.read()
+#        return(rf.tell())
