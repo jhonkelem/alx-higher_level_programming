6f284e8e1f2d3b1b0d54d38fc6e22d321c1e9e11
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-09-06 20:44:26 +0200

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,27 +1,19 @@
 #!/usr/bin/node
-
 /**
- * Represents a parallelogram with 4 right angles
+ * Represents a parallelogram with 4 right angles.
  */
-
 class Rectangle {
-
-	/**
-	 * Creates a new Rectangle with the given dimensions.
-	 * @param {Number} w the value of the width.
-	 * @param {Number} h the value of the height.
-	 */
-
-	constructor (w, h) {
-
-	  if (w > 0 && h > 0) {
-
-		this.width = w;
-		this.height = h;
-
-	}
-	}
-
+  /**
+   * Creates a new Rectangle with the given dimensions.
+   * @param {Number} w The value of the width.
+   * @param {Number} h The value of the height.
+   */
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 
 module.exports = Rectangle;
