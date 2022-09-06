2cac18f693a5d5aa0005f6e7d90ed2e59e0d954e
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-09-06 20:49:22 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,11 +4,10 @@ const dict = require('./101-data.js').dict;
 const newDict = {};
 
 Object.getOwnPropertyNames(dict).forEach(occurences => {
-	if (newDict[dict[occurences]] === undefined) {
-		newDict[dict[occurences]] = [occurences];
-	} else {
-		newDict[dict[occurences]].push(occurences);
-
-	}
-       });
-       console.log(newDict);
+  if (newDict[dict[occurences]] === undefined) {
+    newDict[dict[occurences]] = [occurences];
+  } else {
+    newDict[dict[occurences]].push(occurences);
+  }
+});
+console.log(newDict);
