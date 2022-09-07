58013de64abb52e64ab2ea52fd085024b0e98b00
Yohannes Kelem <99354658+jhonkelem@users.noreply.github.com>
2022-09-07 14:25:05 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const texts = [
-
-	  'C is fun',
-
-	  'Python is cool',
-
-	  'JavaScript is amazing'
-
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let i = 0; i < texts.length; i++) {
-	console.log(texts[i]);
+  console.log(texts[i]);
 }
